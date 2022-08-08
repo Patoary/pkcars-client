e1079e29d616b782458ba6a51aa80e255ec36a3f
patoary <zahidsagar93@gmail.com>
2022-08-09 00:12:20 +0900

    check

--- src/pages/MyItem/MyItemCard.js
+++ src/pages/MyItem/MyItemCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MyItemCard = () => {
     return (
         <div>
-           <h3>detail</h3> 
+           <h3>Detail about my item</h3> 
         </div>
     );
 };
